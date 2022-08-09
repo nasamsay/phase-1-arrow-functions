319e79b75b74398b79fec52fd4e8f24c1aff7104
Nasam Sayour <nasamsay@gmail.com>
2022-08-09 22:29:34 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// Division Expression Function
+const divide = function(a,b) {
+    return a/b
+}
+console.log(divide(45,5))
+
+//Square Arrow Function
+const square = a => a*a
+console.log(square(2))
+
+//Addition Arrow Function
+const add = (a,b) => a+b
+console.log(add(4,7))
